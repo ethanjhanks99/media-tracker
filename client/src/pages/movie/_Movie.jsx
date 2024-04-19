5a98f608d9153801acb4ec7dd72bb378916e9df8
Ethan Hanks <a02244523@aggies.usu.edu>
2024-04-19 14:31:05 -0600

    added information

--- client/src/pages/movie/_Movie.jsx
+++ client/src/pages/movie/_Movie.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { useMovie } from "../../utils/use_movie";
 
@@ -10,13 +10,39 @@ export const Movie = () => {
     return null;
   };
 
-
-  console.log(movieData);
-
   return (
     <>
-      <h2>{movieData.title}</h2>
-      <img src={"https://image.tmdb.org/t/p/original" + movieData.poster_path} alt="" />
+      <h1>{movieData.title}</h1>
+      <div>
+        <img src={"https://image.tmdb.org/t/p/original" + movieData.poster_path} alt="" />
+      </div>
+      <div className="information">
+        <div className="overview">
+          <h3>Overview</h3>
+          {movieData.overview}
+        </div>
+        <div className="genre">
+          <h3>Genre</h3>
+          {movieData.genres.map((genre) => {
+            return (
+              <div key={genre.id} className="genre">{genre.name}</div>
+            )
+          })}
+        </div>
+        <div>
+          <h3>Production Companies</h3>
+          {movieData.production_companies.map((company) => {
+            return (
+              <div key={company.id}>
+                <img src={"https://image.tmdb.org/t/p/original" + company.logo_path} alt="" />
+                <div className="name">
+                {company.name}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </>
   )
 }
