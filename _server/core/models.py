4974586d9336773f3136e027fcdc2c8c575461ae
Ethan Hanks <a02244523@aggies.usu.edu>
2024-04-23 10:19:02 -0600

    gonna branch

--- _server/core/models.py
+++ _server/core/models.py
@@ -5,14 +5,14 @@ from django.contrib.auth.models import User
 class Movies(models.Model):
     movie_id = models.IntegerField(primary_key=False)
     title = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ManyToManyField(User)
 
 class Shows(models.Model):
     show_id = models.IntegerField(primary_key=False)
     title = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ManyToManyField(User)
 
 class Games(models.Model):
     show_id = models.IntegerField(primary_key=False)
     title = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ManyToManyField(User)
